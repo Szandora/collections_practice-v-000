ac2d9e804c77e2e8fe07440b9ea23367f0bfdf0b
Sheila Wittman <szandora789@gmail.com>
2018-06-09 15:07:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,3 +14,80 @@ def sort_array_desc(array)
 end
 array
 end
+
+
+
+def sort_array_char_count(array)
+  array = array.new 
+  array.length 
+  sort array length 
+end
+array
+end
+
+
+
+def swap_elements(array)
+  array = array.new 
+  array swap index[1] and index[2]
+end
+array
+end
+
+
+
+def swap_elements_from_to(array)
+  array = ["a", "b", "c"]
+  swap_elements_from_to(["a", "b", "c"],2,1)
+end
+array
+end
+
+
+
+def reverse_array(array)
+  array = array.new
+  array.reverse 
+end
+array
+end
+
+
+
+def kesha_maker(array)
+  array = array.new 
+array.each do |something|
+  something = $
+end
+array
+end
+
+
+
+def find_a(array)
+  array = array.new
+  array.find do |starts with "a"|
+  end
+  array
+end
+
+
+
+def sum_array(array)
+  array = array.new
+  array adds 
+end
+array
+end
+
+
+
+def add_s(array)
+  array = array.new
+  add s to each element except 1
+end
+array 
+end
+
+
+
